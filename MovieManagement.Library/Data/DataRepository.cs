4d89cabab67b50cd3d03ecbea59916edfe4b1afb
harunaabaldeh <harunaabaldeh@gmail.com>
2022-06-16 10:12:08 +0100

    Add project files.

---
+++ MovieManagement.Library/Data/DataRepository.cs
@@ -0,0 +1,29 @@
+﻿using MovieManagement.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.Library.Data
+{
+    public class DataRepository : IDataRepository
+    {
+        private static List<MovieModel> _movies = new()
+        {
+            new MovieModel { Id = 1, Name = "Vickings", Cost = 22.2m },
+            new MovieModel { Id = 2, Name = "Game of Throne", Cost = 55.2m }
+        };
+
+        public MovieModel AddMovie(MovieModel movie)
+        {
+            _movies.Add(movie);
+            return movie;
+        }
+
+        public List<MovieModel> GetMovies()
+        {
+            return _movies;
+        }
+    }
+}
---
+++ MovieManagement.Library/Handlers/GetMovieByIdHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using MovieManagement.Library.Data;
+using MovieManagement.Library.Models;
+using MovieManagement.Library.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManagement.Library.Handlers
+{
+    public class GetMovieByIdHandler : IRequestHandler<GetMovieByIdQuery, MovieModel>
+    {
+        private readonly IDataRepository _dataRepository;
+        private readonly IMediator _mediator;
+
+        public GetMovieByIdHandler(IDataRepository dataRepository, IMediator mediator)
+        {
+            _dataRepository = dataRepository;
+            _mediator = mediator;
+        }
+
+        public async Task<MovieModel> Handle(GetMovieByIdQuery request, CancellationToken cancellationToken)
+        {
+            var movies = await _mediator.Send(new GetMovieListQuery());
+            var movie = movies.FirstOrDefault(u => u.Id == request.Id);
+
+            return movie;
+        }
+    }
+}
